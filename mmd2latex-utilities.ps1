023cd589b84a6b2ea5d1b523adf3d8f760fd98e0
tianhantang <tianhantang.pd@gmail.com>
2024-03-06 15:56:23 +0900

    Add function convert-cross-reference
    as well as the test file

--- mmd2latex-utilities.ps1
+++ mmd2latex-utilities.ps1
@@ -4,6 +4,7 @@
 	This utility script contains the following functions:
 	- `check-whether-inside-block-comment`
 	- `filter-out-line-comment`
+	- `convert-cross-reference`
 #>
 # ////////////////////////////////////////////////////////////////
 
@@ -81,4 +82,58 @@ function filter-out-line-comment {
 			return $line
 		}
 	}
+}
+
+<#
+	@brief: Converts mmd-style cross-references to corresponding LaTeX-style syntax with appropriate prefixes.
+
+	@details:
+	- This function searches for references in a specific Markdown format, identified by `[@type:label]`, and converts them into a LaTeX-valid format.
+	- It supports different types of references, including figures, equations, tables, and sections, by prefixing them with the corresponding word (Figure, Equation, Table, Section) followed by a LaTeX `\ref{type:label}` command.
+
+	@param[in]:
+	- $line: The line of text to be processed. This line may contain zero or more references in the Markdown format that need to be converted to the LaTeX format.
+
+	@example:
+	- Input: "according to [@eq:01], we know that energy equals to mass times the speed of light squared."
+	- Output: "according to Equation \ref{eq:01}, we know that energy equals to mass times the speed of light squared."
+
+	@date:
+	- created on 2024-02-25
+	- updated on 2024-03-06
+#>
+function convert-cross-reference {
+    param (
+        [string]$line
+    )
+
+    # Define a hashtable to map prefixes to their corresponding text
+    $prefixMap = @{
+        'fig' = 'Figure';
+        'eq'  = 'Equation';
+        'tb' = 'Table';
+        'sec' = 'Section'
+    }
+
+    # Regular expression to match the reference pattern
+    $refPattern = '\[@ref:(\w+):(\w+)\]'
+
+    # Find all matches in the line
+    while ($line -match $refPattern) {
+        $fullMatch = $matches[0]
+        $type = $matches[1]
+        $label = $matches[2]
+
+        # Determine the prefix text based on the type
+        $prefixText = $prefixMap[$type]
+
+        # If the prefix is found, replace the reference with the LaTeX formatted reference
+        if ($prefixText) {
+            $replacement = "$prefixText \ref{${type}:${label}}"
+            $line = $line -replace [regex]::Escape($fullMatch), $replacement
+        }
+		# @todo, there is a potential bug that if the prefix is not found, the program will enter an infinite loop
+    }
+
+    return $line
 }
